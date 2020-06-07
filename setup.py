0fac5b85c685b0d92cce44d7e4746d0586573521
Makoto <FujiMakoto@users.noreply.github.com>
2020-06-06 22:29:23 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
         name='pysaucenao',  # How you named your package folder (MyLib)
         packages=['pysaucenao'],  # Chose the same as "name"
-        version='0.2.1',  # Start with a small number and increase it with every change you make
+        version='0.2.2',  # Start with a small number and increase it with every change you make
         license='gpl-3.0',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
         description='PySauceNao is an unofficial asynchronous library for the SauceNao API. It supports lookups via URL or from the local filesystem.',  # Give a short description about your library
         long_description=long_description,
