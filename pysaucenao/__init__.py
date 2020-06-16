058d3c90764eca7dff85a7e5358aece03435cfbf
Makoto <FujiMakoto@users.noreply.github.com>
2020-06-16 12:59:33 -0400

    Copyright info

--- pysaucenao/__init__.py
+++ pysaucenao/__init__.py
@@ -1,3 +1,12 @@
 from pysaucenao.saucenao import SauceNao
 from pysaucenao.containers import GenericSource, PixivSource, BooruSource, VideoSource, MangaSource
 from pysaucenao.errors import *
+
+__author__      = 'FujiMakoto'
+__copyright__   = 'Copyright 2020, Taiga.sh Development'
+__credits__     = ['FujiMakoto']
+__license__     = 'gpl-3.0'
+__version__     = '1.0.2'
+__maintainer__  = 'FujiMakoto'
+__email__       = 'makoto+github@taiga.sh'
+__status__      = 'Stable'
