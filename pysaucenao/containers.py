0833742deba30c13ddf649118737591d8ee64560
FeeeeK <krmrclemens@gmail.com>
2020-11-25 15:25:15 +0300

    Add urls to GenericSource

--- pysaucenao/containers.py
+++ pysaucenao/containers.py
@@ -228,6 +228,7 @@ class GenericSource:
         self.author_url:    typing.Optional[str] = None
         self.title:         typing.Optional[str] = None
         self.url:           typing.Optional[str] = None
+        self.urls:           typing.Optional[list] = None
         self.index:         typing.Optional[str] = None  # The name of the index pulled from. See INDEXES above
         self.index_id:      typing.Optional[int] = None
         self.index_name:    typing.Optional[str] = None
@@ -289,6 +290,7 @@ class GenericSource:
         # URL to the index page. Booru's may also provide links to the original source, which can be found with source_url
         if 'ext_urls' in data:
             self.url = data['ext_urls'][0]
+            self.urls = data['ext_urls']
 
     def __repr__(self):
         rep = reprlib.Repr()
