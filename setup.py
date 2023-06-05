7eea8a9999a03b3a4c7c31da732b88a4953e499e
rainyDayDevs <7929996+rainyDayDevs@users.noreply.github.com>
2023-06-05 03:19:52 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
         name='pysaucenao',  # How you named your package folder (MyLib)
         packages=['pysaucenao'],  # Chose the same as "name"
-        version='1.6.1',  # Start with a small number and increase it with every change you make
+        version='1.6.2',  # Start with a small number and increase it with every change you make
         license='gpl-3.0',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
         description='PySauceNao is an unofficial asynchronous library for the SauceNao API. It supports lookups via URL or from the local filesystem.',  # Give a short description about your library
         long_description=long_description,
@@ -18,7 +18,7 @@ setup(
         author='Makoto',  # Type in your name
         author_email='makoto+github@taiga.sh',  # Type in your E-Mail
         url='https://github.com/FujiMakoto/pysaucenao',  # Provide either the link to your github or to your website
-        download_url='https://github.com/FujiMakoto/pysaucenao/archive/1.6.1.tar.gz',
+        download_url='https://github.com/FujiMakoto/pysaucenao/archive/1.6.2.tar.gz',
         keywords=['saucenao', 'anime', 'artwork'],  # Keywords that define your package best
         install_requires=[
             'aiohttp',
